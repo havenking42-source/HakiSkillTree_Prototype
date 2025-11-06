8ac958be061435794fb8131f5ae1292a3f0ff3fc
Nicholas <naw7961@rit.edu>
2025-11-06 13:13:21 -0700

    Start initial character controls

---
+++ app/layouts/character-controls.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  <UForm>
+    <UFormField label="Character Name">
+      <UInput placeholder="Enter Character Name"/>
+    </UFormField>
+    <UFormField label="Initial Haki Pool">
+      <UInput type="number"/>
+    </UFormField>
+    <UFormField label="Initial Haki Cap">
+      <Uinput type="number"/>
+    </UFormField>
+    <UFormField label="Total Haki Points">
+      <UInput type="number"/>
+    </UFormField>
+  </UForm>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/content'
+  ]
 })
